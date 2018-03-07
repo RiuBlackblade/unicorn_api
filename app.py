cdf8804e74907c22139a506bf19a0f906bc6f047
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-03-07 00:52:04 +0000

    Dude this is webscale

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'a9f9ca3f079050626ae4e82e5eab72fa'
-__version__ = '0b9a663b689335581c73beb8af037490'
+__vcs_id__ = '399cb6ec3331ba852ccf7757e4ffb59d'
+__version__ = 'f76234eb9969b087d63206a261e2fe9c'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
